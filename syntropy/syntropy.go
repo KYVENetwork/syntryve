6133977c0aa5b052ea89184be2cc3bd771730800
Troy Kessler <troy.kessler99@gmail.com>
2024-01-22 13:48:55 +0100

    feat: added jetstream nats client

--- syntropy/syntropy.go
+++ syntropy/syntropy.go
@@ -3,18 +3,25 @@ package syntropy
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
-	"github.com/KYVENetwork/syntropy-demo/types"
 	"github.com/KYVENetwork/syntropy-demo/utils"
 	"github.com/SyntropyNet/pubsub-go/pubsub"
-	"github.com/goccy/go-json"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/nats-io/nats.go"
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 	"time"
+)
+
+const (
+	exampleStreamName  = "osmosis-stream"
+	uniqueConsumerName = "troykessler"
+	streamSize         = 20_000
+	streamTTL          = 24 * time.Hour
 )
 
 func StartSyntropyWS(accessToken, natsUrl, streamUrl, dbPath string, debug bool) {
@@ -22,21 +29,46 @@ func StartSyntropyWS(accessToken, natsUrl, streamUrl, dbPath string, debug bool)
 	if debug {
 		println(jwt)
 	}
-	// Set user credentials and options for NATS connection.
-	opts := []nats.Option{}
-	opts = append(opts, nats.UserJWTAndSeed(jwt, accessToken))
 
-	// Connect to the NATS server using the provided options.
-	service := pubsub.MustConnect(
-		pubsub.Config{
-			URI:  natsUrl,
-			Opts: opts,
-		})
-	log.Println("Connected to NATS server.")
+	nc, err := nats.Connect(natsUrl,
+		nats.DrainTimeout(10*time.Second),
+		nats.UserJWTAndSeed(jwt, accessToken),
+	)
+	if err != nil {
+		fmt.Println("failed to connect ", err)
+	}
+	defer nc.Close()
+	fmt.Println("Connected to NATS server.")
 
-	// Create a context with a cancel function to control the cancellation of ongoing operations.
+	js, err := nc.JetStream()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
+	connCtx, connCancelFn := context.WithTimeout(ctx, 10*time.Second)
+	defer connCancelFn()
+
+	_, err = js.AddStream(&nats.StreamConfig{
+		Name:     exampleStreamName,
+		Subjects: []string{streamUrl},
+		MaxMsgs:  streamSize,
+		MaxAge:   streamTTL,
+	}, nats.Context(connCtx))
+	if err != nil {
+		fmt.Printf("Error during adding of JetStream %s: %s", exampleStreamName, err)
+	}
+	fmt.Printf("JetStream %s added successfully.\n", exampleStreamName)
+
+	_, err = js.AddConsumer(exampleStreamName, &nats.ConsumerConfig{
+		Durable: uniqueConsumerName,
+	})
+	if err != nil {
+		fmt.Printf("Error during adding consumer of JetStream %s: %s", exampleStreamName, err)
+	}
+	fmt.Printf("Consumer to stream %s added successfully.\n", exampleStreamName)
 
 	// Set up DB
 	db, err := sql.Open("sqlite3", dbPath)
@@ -62,44 +94,61 @@ func StartSyntropyWS(accessToken, natsUrl, streamUrl, dbPath string, debug bool)
 		panic(err)
 	}
 
-	// Add a handler function to process messages received on the exampleSubscribeSubject.
-	service.AddHandler(streamUrl, func(data []byte) error {
-		var message types.Message
-		if err := json.Unmarshal(data, &message); err != nil {
-			panic(fmt.Sprintf("failed to unmarshal message: %w", err))
-		}
+	subscription, err := js.PullSubscribe(streamUrl, uniqueConsumerName, nats.ManualAck(), nats.Bind(exampleStreamName, uniqueConsumerName))
+	if err != nil {
+		fmt.Print("Error during pull subscription: ", err)
+	}
 
-		if debug {
-			log.Println(string(data))
-		}
-		var created = time.Now().UTC()
-		uId := utils.CreateSha256Checksum(append([]byte(created.String()), data...))
+	go func() {
+		for {
+			msgs, err := subscription.Fetch(1, nats.Context(ctx))
+			if err != nil {
+				if errors.Is(err, context.Canceled) {
+					return
+				}
+				fmt.Print("Error during pulling next message: ", err)
+			}
+			msg := msgs[0]
 
-		result, err := stmt.Exec(uId, created, data)
-		if err != nil {
-			panic(err)
-		}
+			timestamp, err := strconv.ParseInt(msg.Header.Get("timestamp"), 10, 64)
+			if err != nil {
+				panic(err)
+			}
 
-		// Check the number of rows affected to determine if the insert was successful
-		rowsAffected, _ := result.RowsAffected()
-		if rowsAffected == 0 {
-			log.Println("No rows were affected, insert may have failed")
-			os.Exit(1)
+			created := time.Unix(timestamp/1e9, 0)
+			uId := utils.CreateSha256Checksum(append([]byte(created.String()), msg.Data...))
+
+			fmt.Println("Received message ", created, " with size ", len(msg.Data))
+
+			if debug {
+				fmt.Println(string(msg.Data))
+			}
+
+			result, err := stmt.Exec(uId, created, msg.Data)
+			if err != nil {
+				panic(err)
+			}
+
+			// Check the number of rows affected to determine if the insert was successful
+			rowsAffected, _ := result.RowsAffected()
+			if rowsAffected == 0 {
+				log.Println("No rows were affected, insert may have failed")
+				os.Exit(1)
+			}
+
+			if err := msg.Ack(); err != nil {
+				fmt.Println("Error during message ack: ", err)
+			}
 		}
-		return nil
-	})
+	}()
 
 	// Set up signal handling to gracefully shut down when receiving SIGINT or SIGTERM signals.
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		<-signalChan
-		cancel()
-	}()
-
-	// Start serving messages and processing them using the registered handler function.
-	if err := service.Serve(ctx); err != nil {
-		println("\nStopped syntryve")
-		os.Exit(1)
+	<-signalChan
+	if err := nc.Drain(); err != nil {
+		fmt.Println("Error during connection close: ", err)
 	}
+
+	fmt.Println("Subscription stopped")
 }
